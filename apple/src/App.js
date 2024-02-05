738223ed3b429cdec00735f71ed851e97c5d9e96
samade747 <samad.e747@gmail.com>
2024-02-05 22:41:22 +0500

    1041PM 5 2 2024

--- apple/src/App.js
+++ apple/src/App.js
@@ -15,8 +15,12 @@ function App() {
       <div className='inner-container'> 
 
       <div className='botton-change'>
-    <button onClick={() => setColor('red') }>Red</button>
-    <button onClick={() => setColor('white') }>white</button>
+    <button onClick={() => setColor('red') } style={{backgroundColor: 'red', color: "white"}}  >Red</button>
+    <button onClick={() => setColor('white')} style={{backgroundColor: "white", color: "black"}}  >white</button>
+    <button onClick={() => setColor('black')} style={{backgroundColor: "black", color: "white"}}  >black</button>
+    <button onClick={() => setColor('orange')} style={{backgroundColor: "orange", color: "white"}}  >orange</button>
+    <button onClick={() => setColor('purple')} style={{backgroundColor: "purple", color: "white"}}  >purple</button>
+    <button onClick={() => setColor('pink')} style={{backgroundColor: 'pink', color: "white"}} >pink</button>
         </div>        
        </div>
     </div>

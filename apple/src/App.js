4a375f95ab4b98007394ec2fbda2113f48c30cac
samade747 <samad.e747@gmail.com>
2024-02-06 14:27:12 +0500

    226PM

--- apple/src/App.js
+++ apple/src/App.js
@@ -10,17 +10,17 @@ function App() {
 
   return (
     <>
-    <div className="App" style={{backgroundColor: color}} >
+    <div className="App" style={{backgroundColor: color, border: "10px solid white"}} >
       
       <div className='inner-container'> 
 
       <div className='botton-change'>
-    <button onClick={() => setColor('red') } style={{backgroundColor: 'red', color: "white"}}  >Red</button>
-    <button onClick={() => setColor('white')} style={{backgroundColor: "white", color: "black"}}  >white</button>
-    <button onClick={() => setColor('black')} style={{backgroundColor: "black", color: "white"}}  >black</button>
-    <button onClick={() => setColor('orange')} style={{backgroundColor: "orange", color: "white"}}  >orange</button>
-    <button onClick={() => setColor('purple')} style={{backgroundColor: "purple", color: "white"}}  >purple</button>
-    <button onClick={() => setColor('pink')} style={{backgroundColor: 'pink', color: "white"}} >pink</button>
+    <button onClick={() => setColor('red') } style={{backgroundColor: 'red', color: "white", borderRadius: "30px"  }}  >Red</button>
+    <button onClick={() => setColor('white')} style={{backgroundColor: "white", color: "black", borderRadius: "30px"  }}  >white</button>
+    <button onClick={() => setColor('black')} style={{backgroundColor: "black", color: "white", borderRadius: "30px"  }}  >black</button>
+    <button onClick={() => setColor('orange')} style={{backgroundColor: "orange", color: "white", borderRadius: "30px"  }}  >orange</button>
+    <button onClick={() => setColor('purple')} style={{backgroundColor: "purple", color: "white", borderRadius: "30px"  }}  >purple</button>
+    <button onClick={() => setColor('pink')} style={{backgroundColor: 'pink', color: "white", borderRadius: "30px"  }} >pink</button>
         </div>        
        </div>
     </div>
